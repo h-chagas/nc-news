8b36042e1a91f545514f616dcdac588823d8e375
h-chagas <hugochagasuk@gmail.com>
2022-09-26 15:18:40 +0100

    create header and footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>I am the footer</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+
+
+    return (
+        <header>
+            <h1>NC News</h1>
+        </header>
+    )
+}
+
+export default Header;
