58e295014398ebf2e0d946ccdd16434bbedc1b75
h-chagas <hugochagasuk@gmail.com>
2022-09-27 16:43:03 +0100

    move useState to a component below and create a new Route with a parametric endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Articles from './components/Articles';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Nav from './components/Nav';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Articles from "./components/Articles";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Topics from "./components/Topics";
 
 function App() {
 
-  const [articles, setArticles] = useState([])
+   return (
+      <div className="App">
+         <Header />
+         <Nav />
 
-  return (
-    <div className="App">
-      <Header />
-      <Nav />
+         <Routes>
+            <Route path="/" element={<p>Home</p>} />
+            <Route
+               path="/topics"
+               element={<Topics/>}
+            />
+            <Route
+               path="/topics/:topic_slug"
+               element={<Articles />}
+            />
+            <Route
+               path="/articles"
+               element={<Articles/>}
+            />
+            <Route path="/users" element={<p>Users</p>} />
+            <Route path="*" element={<p>404 not found</p>} />
+         </Routes>
 
-      <Routes>
-        <Route path='/' element={<p>Home</p>} />
-        <Route path='/topics' element={<p>Topics</p>} />
-        <Route path='/articles' element={<Articles articles={articles} setArticles={setArticles} />} />
-        <Route path='/users' element={<p>Users</p>} />
-        <Route path='*' element={<p>404 not found</p>} />
-      </Routes>
-
-      <Footer />
-    </div>
-  );
+         <Footer />
+      </div>
+   );
 }
 
 export default App;
