7fce9ba32779a4794bf04d29452dc0eabc29931e
h-chagas <hugochagasuk@gmail.com>
2022-09-28 12:10:56 +0100

    add Link to article title

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import styles from "../styles/SingleArticle.module.css";
 
 const SingleArticle = ({ article, articles }) => {
    return (
       <article className={styles.article}>
-         <h2 className={styles.article_title}>{article.title}</h2>
+         <Link to={`/articles/${article.article_id}`}>
+            <h2 className={styles.article_title}>{article.title}</h2>
+         </Link>
          <p className={styles.article_author}>Article by {article.author}</p>
          <p className={styles.article_date}>Created at {article.created_at}</p>
          <p className={styles.article_topic}>Topic: {article.topic}</p>
@@ -11,6 +14,7 @@ const SingleArticle = ({ article, articles }) => {
          <p className={styles.article_comment}>Comments: {article.comment_count}</p>
          <p className={styles.article_votes}>Votes: {article.votes}</p>
       </article>
+      
    );
 };
 
