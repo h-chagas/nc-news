4e5adbb4d347153c867e375e6ac544ad3cabb53e
h-chagas <hugochagasuk@gmail.com>
2022-09-29 13:49:04 +0100

    refactor CommentList component in order to render comments dynamically

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,22 +1,47 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styles from '../styles/CommentsList.module.css'
+import { showCommentsByArticle } from '../utils/api';
+import Error404 from './Error404';
+import SingleComment from './SingleComment';
+
 
 const CommentsList = () => {
 
+    const [comments, setComments] = useState([])
+    const [error, setError] = useState(null);
+
+   const { article_id } = useParams();
+
+
+    useEffect(() => {
+        showCommentsByArticle(article_id)
+        .then((data) => {
+            console.log(data.comments);
+            setComments(data.comments)
+        })
+        .catch((err) => {
+            setError(err);
+         });
+    }, [article_id])
+
+    if (error) {
+        return <Error404 />;
+     }
+
     return (
-        <ul className={styles.ul}>
-            <li className={styles.li}>
-                Comment
-                <span>Vote</span>
-            </li>
-            <li className={styles.li}>
-                Comment
-                <span>Vote</span>
-            </li>
-            <li className={styles.li}>
-                Comment
-                <span>Vote</span>
-            </li>
-        </ul>
+        <>
+        <h3>Comments</h3>
+        <ol className={styles.ul}>
+            {
+                comments.map((comment) => {
+                    return (
+                        <SingleComment comment={comment}/>
+                    )
+                })
+            }
+        </ol>
+        </>
     )
 
 }
