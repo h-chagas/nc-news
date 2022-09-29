5f99b5617e8dde3b76e2dd5f138eeee0837f8f11
h-chagas <hugochagasuk@gmail.com>
2022-09-29 13:47:08 +0100

    create SingleComment component and its style to render comments for each article

---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,21 @@
+import styles from '../styles/SingleComment.module.css'
+
+const SingleComment = ({comment}) => {
+   return (
+      <li key={comment.comment_id} className={styles.li} >
+         <p className={styles.body}>{comment.body}</p>
+         <p className={styles.author}>{comment.author}</p>
+         <p className={styles.date}>Posted on {comment.created_at}</p>
+         <p className={styles.votes}>{comment.votes} votes</p>
+         <div className={styles.voteCommentDiv}>
+            <p>Vote</p>
+             <div className={styles.btnDiv}>
+                 <button className={styles.leftBtn}>+</button>
+                 <button className={styles.rightBtn}>-</button>
+             </div>
+         </div>
+      </li>
+   );
+};
+
+export default SingleComment;
