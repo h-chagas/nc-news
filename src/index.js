a33e2c397ba6dea941ad0524b3432878581cdd60
h-chagas <hugochagasuk@gmail.com>
2022-09-30 14:51:16 +0100

    import and use UserProvider, wrapping my App component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./context/Users";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <BrowserRouter>
-      <App />
-   </BrowserRouter>
+   <UserProvider>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </UserProvider>
 );
